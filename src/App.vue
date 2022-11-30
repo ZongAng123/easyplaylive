6010c0a528b633459b11109801c6bd63326b9220
纵昂 <zongang15@yahoo.com>
2022-11-30 16:07:32 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="EasyPlayer示例"/>
   </div>
 </template>
 
